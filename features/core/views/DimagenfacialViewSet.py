3a3284cb04bc0164a264e96fe45b60c8c7d679a3
lchang95 <lchang@defontana.com>
2023-11-19 22:53:42 -0500

    Fix update imagenfacial

--- features/core/views/DimagenfacialViewSet.py
+++ features/core/views/DimagenfacialViewSet.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
+from django.shortcuts import get_object_or_404
 from django.utils.encoding import force_bytes
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import viewsets, filters, status
+from rest_framework import filters, status
 from rest_framework.parsers import MultiPartParser, JSONParser
 from django.db import transaction
 from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 
 from features.core.Filters.DimagenfacialFilter import DimagenfacialFilter
 from features.core.models import Dimagenfacial
@@ -15,7 +17,7 @@ from features.core.models import Dciudadano  # Asegúrate de importar el modelo 
 
 
 
-class DimagenfacialViewSet(viewsets.ModelViewSet):
+class DimagenfacialViewSet(ModelViewSet):
     queryset = Dimagenfacial.objects.using('enrrolamiento').all()
     serializer_class = DimagenfacialSerializer
     permission_classes = (DimagenfacialPermissions,)
@@ -44,3 +46,27 @@ class DimagenfacialViewSet(viewsets.ModelViewSet):
 
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    @transaction.atomic
+    def update(self, request, *args, **kwargs):
+        serializer = self.get_instance(request, kwargs)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @transaction.atomic
+    def partial_update(self, request, *args, **kwargs):
+        serializer = self.get_instance(request, kwargs)
+        return Response(serializer.data, status = status.HTTP_200_OK)
+
+    def get_instance(self, request, kwargs):
+        pk = kwargs['pk']
+        instance = get_object_or_404(self.queryset, pk=pk)
+        self.check_object_permissions(request, instance)
+        serializer = DimagenfacialSerializer(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+
+        # Asegúrate de que el campo 'foto' esté en la solicitud como archivo binario
+        foto = force_bytes(request.data.get('foto'))
+
+        serializer.save(foto=foto)
+
+        return Response(serializer.data)
--- features/core/views/user_api.py
+++ features/core/views/user_api.py
@@ -1,11 +1,10 @@
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import ModelViewSet
 
 from features.core.permissions.UserPermissions import UserPermissions
 from features.core.serializers import *
-from rest_framework import viewsets, status, exceptions
+from rest_framework import status, exceptions
 from django.db import transaction
 from django.shortcuts import get_object_or_404
 
@@ -25,30 +24,12 @@ class UserApiView(ModelViewSet):
     search_fields = ('phone_number', 'username', 'email', 'first_name', 'last_name',)
     ordering_fields = ('phone_number', 'username', 'email', 'first_name', 'last_name',)
 
-    # def get_permissions(self):
-    #     if self.action == 'create':
-    #         return [AllowAny()]
-    #     return super(UserApiView, self).get_permissions()
-
     @action(detail=False, methods=['get'])
     def me(self, request):
         user = request.user
         self.kwargs = {'pk': user.id}
         obj = self.get_serializer(self.get_object()).data
         return Response(obj, status=status.HTTP_200_OK)
-
-    # @transaction.atomic
-    # def create(self, request, *args, **kwargs):
-    #     serializer = self.get_serializer(data=request.data)
-    #     serializer.is_valid(raise_exception=True)
-    #     user = serializer.save(is_active=False)
-    #     user.set_password(request.data.get('password'))
-    #
-    #     user.is_active = True
-    #     user.save()
-    #
-    #     headers = self.get_success_headers(serializer.data)
-    #     return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
     @transaction.atomic
     def update(self, request, *args, **kwargs):
